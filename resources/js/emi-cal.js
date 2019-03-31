b23f139caa390791cc3695abc784709df0415af2
unknown <hp.patel97@gmail.com>
2019-03-31 17:26:41 -0400

    This is git push

--- resources/js/emi-cal.js
+++ resources/js/emi-cal.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-  
+  //set and get values on slider 
   $("input[type='range']").change(function() {
-    alert($("input:range").val());
-    $("input:range").val();
-    $('input:range').val( $("input:range").val());
+      //loan-amont  
+      $("#loan-text").val( $("#loan-amount-slider").val());
 
+    //intrest
+    $("#intrest-rate-text").val( $("#intrest-rate-slider").val());
+      
   })
+   //set and get values on slider 
+  $("input[type='text']").change(function() {
+      //loan-amont  
+    $("#loan-amount-slider").val($("#loan-text").val());
+    //intrest 
+    $("#intrest-rate-slider").val($("#intrest-rate-text").val());
+    
+  })
+
 });
 
