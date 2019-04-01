75eab8d813ba0db236c3abd2508b93aa7eb1ec90
unknown <hp.patel97@gmail.com>
2019-04-01 01:35:26 -0400

    changes in emi cal

--- resources/js/emi-cal.js
+++ resources/js/emi-cal.js
@@ -1,29 +1,34 @@
 $(document).ready(function() {
   $(".range-slider-month").hide();
-  function resetBtn() {
-    $(".btn-year").addClass('btn-active');
-    $(".btn-year").removeClass('btn-default');
-  }
+  $("#loan-tenure-month-text").hide();
+  
   $(".btn-year").click(function () {
-    $("#loan-tenure-text").val("0");
-    $("#loan-tenure-year-slider").val("0");
     
-    
-      //hide-month && Display Year 
+    //hide-month && Display Year 
       $(".range-slider-month").hide();
+      $("#loan-tenure-month-text").hide();
+
       $(".range-slider-year").show();
+      $("#loan-tenure-year-text").show();
+      
+      
       //Add Both classes
+     
       $(".btn-month").removeClass('btn-primary');
       $(".btn-month").addClass('btn-default');
+      
       $(".btn-year").addClass('btn-primary');
       $(".btn-year").removeClass('btn-default');
     
   });
   $(".btn-month").click(function () {
-    $("#loan-tenure-text").val("0");
-    $("#loan-tenure-month-slider").val("0");
-    $("#loan-tenure-text").val( $("#loan-tenure-year-slider").val());
-    
+    //hide-month && Display Year 
+      $(".range-slider-year").hide();
+      $("#loan-tenure-year-text").hide();
+      
+      $(".range-slider-month").show();
+      $("#loan-tenure-month-text").show();
+
     
     //hide-month && Display Year 
     $(".range-slider-year").hide();
@@ -39,23 +44,30 @@ $(document).ready(function() {
   
   //set and get values on slider 
   $("input[type='range']").change(function() {
-      //loan-amont  
+     //loan-amont  
       $("#loan-text").val( $("#loan-amount-slider").val());
 
     //intrest
     $("#intrest-rate-text").val( $("#intrest-rate-slider").val());
     
     //loan-tenure-year-slider
-    $("#loan-tenure-text").val( $("#loan-tenure-year-slider").val());
+    $("#loan-tenure-year-text").val( $("#loan-tenure-year-slider").val());
     
-    $("#loan-tenure-text").val( $("#loan-tenure-month-slider").val());
+    //loan-tenure-month-slider
+    $("#loan-tenure-month-text").val( $("#loan-tenure-month-slider").val());
   })
    //set and get values on slider 
-  $("input[type='text']").change(function() {
+  
+  
+  
+   $("input[type='text']").change(function() {
       //loan-amont  
     $("#loan-amount-slider").val($("#loan-text").val());
     //intrest 
     $("#intrest-rate-slider").val($("#intrest-rate-text").val());
+    ////loan-tenure-year-slider
+    $("#loan-tenure-year-slider").val($("#loan-tenure-year-text").val());
+    $("#loan-tenure-month-slider").val($("#loan-tenure-month-text").val());
     
   })
 
